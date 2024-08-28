5dab682997cbcc8724c7ff08dd935cb9a9533a3b
brubiz <linsbruno086@gmail.com>
2024-08-28 14:14:14 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
 const botao = document.getElementById('meuBotao');
+const mensagem = document.getElementById('mensagem');
+
 
 botao.addEventListener('click', function() {
-  
-    alert('EuTeAmo meu gostoso!');
+
+    mensagem.textContent = 'EuTeAmo meu gostoso!';
 });
+
